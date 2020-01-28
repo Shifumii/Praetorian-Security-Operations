3b044fce288815d0d7c3d1ec9f01e41eaadc4896
Shifumii <57402229+Shifumii@users.noreply.github.com>
2020-01-28 10:45:33 +0100

    PSO CUP UH60 fix

--- PSO-specific Addons/pso_cup_uh60/config.cpp
+++ PSO-specific Addons/pso_cup_uh60/config.cpp
@@ -25,8 +25,7 @@ class CfgVehicles
 	class CUP_B_UH60M_Unarmed_US;
 	class CUP_B_UH60M_US;
 	
-	class CUP_B_UH60M_Unarmed_US_Black_PSO: CUP_B_UH60M_Unarmed_US;
-	{
+	class CUP_B_UH60M_Unarmed_US_Black_PSO: CUP_B_UH60M_Unarmed_US {
 		scope = 2;
 		scopeArsenal = 2;
 		scopeCurator = 2;
